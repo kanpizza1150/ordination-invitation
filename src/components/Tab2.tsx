77ee6616617d7c6a004a52c00532ffe3b373f058
kanpizza <kanpizza@Kanpichchas-Mac-mini.local>
2024-02-11 17:27:16 +0700

    max h

--- src/components/Tab2.tsx
+++ src/components/Tab2.tsx
@@ -3,12 +3,12 @@ const Tab2 = () => {
     <div className="flex flex-col gap-4">
       <div className="flex flex-col gap-2">
         <div className="bg-gold-1 text-white rounded-full text-center p-1 ">
-          ๖ มิถุนายน ๒๕๖๗
+          วันพฤหัสบดีที่ ๖ มิถุนายน ๒๕๖๗
         </div>
       </div>
       <div className="flex flex-col gap-2">
         <div className="bg-gold-1 text-white rounded-full text-center p-1 ">
-          ๗ มิถุนายน ๒๕๖๗
+          วันศุกร์ที่ ๗ มิถุนายน ๒๕๖๗
         </div>
       </div>
     </div>
